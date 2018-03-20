d62bedbfcfa94b7cb979ac407952dfefa5dcd849
Simeon-off <s.simeonoff@gmail.com>
2018-03-20 11:58:51 +0100

    ok

--- myscripts.js
+++ myscripts.js
@@ -60,7 +60,6 @@ function filterSelection(id) {
     displayedPreludes=0;
     for (i = 0; i < x.length; i++) {
       w3RemoveClass(x[i], "show");
-      $(x[i]).fadeOut();
       show = true;
       for (j = 0; j < y.length; j++) {
         if (x[i].className.indexOf(y[j].id) > -1) {
@@ -68,7 +67,6 @@ function filterSelection(id) {
         }
         if (show == true) {
           w3AddClass(x[i], "show");
-          $(x[i]).fadeIn();
           if (x[i].classList.contains("corporation")) {
             displayedCorporations++;
           }
@@ -96,10 +94,8 @@ function myFunction(id) {
   for (i = 0; i < li.length; i++) {
     a = li[i];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].fadeIn;
       li[i].style.display = "";
     } else {
-      li[i].fadeOut;
       li[i].style.display = "none";
     }
   }
