2735a758e8bfc53899b6505e0ca4f5ac4baccd30
Simeon-off <s.simeonoff@gmail.com>
2018-09-24 01:00:15 +0200

    stats js

--- stats.js
+++ stats.js
@@ -21,19 +21,37 @@ r1.on('value', (snap) => {
     return el.players == 2
   });
 
-  gamesWithCorp = games.filter(function(el) {
-    return el.corporations == ["APHRODITE", "CREDICOR"]
+  gamesMap = games.filter(function(el) {
+    return el.map == "THARSIS"
   });
-  console.log(games);
 
-  console.log(gamesWithCorp);
-
-   console.log(games.find("ECOLINE"));
-
+  console.log("ok");
+  newArray2 = generateGameStats(games2p, "APHRODITE");
 })
 
-function generateGameStats (gamesPerPlayers, corporations) {
+function generateGameStats (gamesPerPlayers, corporationName) {
   //returning html formatted text
-
-
+  newArray = [];
+  for (i = 0; i < gamesPerPlayers.length; i++) {
+    var corpsArray = gamesPerPlayers[i]["corporations"];
+    if (corpsArray == undefined) {} //to chatch firebase errors
+    else {
+      if (corpsArray.indexOf(corporationName) > -1) {newArray.push(games2p[i]);}
+      }
+    }
+  totalGames = newArray.length;
+  totalWins = 0;
+  console.log(newArray);
+  for (i = 0; i < totalGames; i++) {
+    //checking if that corporation is the winner
+    var corpsArray = newArray[i]["corporations"];
+    var scoresArray = newArray[i]["scores"];
+    winCorpIndex = corpsArray.indexOf(corporationName);
+    winScoreIndex = 0;
+    previousScore = 0;
+    //findind the winning score index
+    //for (j = 0; 0 < scoresArray.length; j++) {
+      //if (scoresArray[j] > previousScore) {winScoreIndex = j; previousScore = scoresArray[j]; console.log(j);}
+    //}
+  }
 }
