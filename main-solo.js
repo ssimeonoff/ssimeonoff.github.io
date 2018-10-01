3e238b1900377104dc96a9c865297883a08dc9e0
Simeon-off <s.simeonoff@gmail.com>
2018-10-02 00:37:00 +0200

    submit solo

---
+++ main-solo.js
@@ -0,0 +1,105 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD6HEAHfcXGN-WrUxSaraO3TYNzGbAr8ts",
+  authDomain: "tm-games1.firebaseapp.com",
+  databaseURL: "https://tm-games1.firebaseio.com",
+  projectId: "tm-games1",
+  storageBucket: "tm-games1.appspot.com",
+  messagingSenderId: "969120080569"
+};
+firebase.initializeApp(config);
+
+// Reference Games collection
+var gamesRef = firebase.database().ref("games-solo");
+//get the games as an array
+var r1 = firebase.database().ref('games-solo')
+r1.on('value', (snap) => {
+  const val = snap.val()
+  const asArray = Object.keys(val)
+    //.map(key => Object.assign({}, val[key], { key }))
+    .map(key => val[key])
+    document.getElementById("title3").innerHTML = asArray.length; //for the odometer counter
+})
+
+//listen for form SUBMIT
+document.getElementById("form").addEventListener("submit", submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  //Get values
+  var corporation = document.getElementById("corporation").value;
+  var score = document.getElementById("corporation-score").value;
+  var expansions = arrayExpansions();
+  var result = document.querySelector("input[name='result']:checked").value;
+
+  // Save Game
+  saveGame(score, corporation, expansions, result);
+
+  //Show aleart
+    document.getElementById("submit").style.opacity = 0;
+    setTimeout(function(){
+      document.getElementById("submit").style.display = "none";
+      document.querySelector(".alert").style.display = "block";
+    }, 300);
+  setTimeout(function(){
+    document.querySelector(".alert").style.opacity = 1;
+  },600);
+  setTimeout(function(){
+  document.querySelector(".alert").style.backgroundPosition = "90% 2px";
+},1100);
+
+
+  //clear form
+  document.getElementById("form").reset();
+  resetAll();
+
+  //hide alert after 3 seconds
+
+  setTimeout(function(){
+    document.querySelector(".alert").style.backgroundPosition = "90% 60px";
+    setTimeout(function(){
+      document.querySelector(".alert").style.opacity = 0;
+    },500);
+    setTimeout(function(){
+    document.querySelector(".alert").style.display = "none";
+    document.getElementById("submit").style.display = "block";
+  }, 800);
+  setTimeout(function(){
+  document.getElementById("submit").style.opacity = 1;
+}, 1100);
+}, 5000);
+
+}
+
+// Save Game to firebasejs
+function saveGame(corporation, score, expansions, result) {
+  var newGameRef = gamesRef.push();
+  newGameRef.set({
+    corporation: corporation,
+    score: score,
+    expansions: expansions,
+    result: result
+  })
+  console.log(corporation + " " + score)
+}
+
+//getting form values
+
+function arrayExpansions() {
+  expansions = [];
+  x = document.querySelectorAll('input[name="expansions"]:checked');
+  for (i=0; i < x.length; i++) {
+      expansions.push(x[i].value);
+  }
+  return expansions;
+}
+
+function resetAll () {
+  //clear the selected colour
+  x = document.querySelectorAll(".change-colours");
+  for (i=0; i < x.length; i++) {
+    x[i].classList.remove("change-colours");
+  }
+}
---
+++ submit-solo.js
@@ -0,0 +1,120 @@
+$("#form").keypress(function (event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+    }
+});
+/////////////////////////////////////////////////////////////////////////////////////
+function checkForm () {
+  //code executed when "submit" is clicked
+  enableSubmit = true;
+  //if corporation is not selected
+  if (document.querySelectorAll("select[class*='change-colour']").length < 1) {
+    enableSubmit = false;
+    document.getElementById("corporation").classList.add("red-outline");
+   };
+  //if score is not selected
+  if (document.querySelectorAll("input[class*='change-colour']").length < 1) {
+    enableSubmit = false;
+    document.getElementById("corporation-score").classList.add("red-outline");
+  };
+  //if result is not selected
+  if (document.querySelectorAll("input[name='result']:checked").length < 1 ) {
+    x = document.querySelectorAll(".btn-result");
+    for (i = 0; i < x.length; i++) {
+      x[i].classList.add("red-outline");
+    }
+    enableSubmit = false;
+  };
+
+
+  //generates the modal text
+  if (enableSubmit) {
+    generateConfirmationText();
+    document.getElementById("modalOne").style.display = "block";
+    setTimeout(function() {document.querySelector(".modal-window").style.transform = "scale(1)";}, 200);
+    } else { document.getElementById("submit").disabled = true;}
+
+  //remove the red outlines after 3 seconds
+  setTimeout(function(){
+    x = document.querySelectorAll(".red-outline");
+    for (i = 0; i < x.length; i++) {
+      x[i].classList.remove("red-outline");
+    }
+    document.getElementById("submit").disabled = false;
+  }, 2000);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+function changeColours (id) {
+  document.getElementById(id).classList.add("change-colours");
+  document.getElementById(id).classList.remove("not-filled");
+}
+
+function changeColours2 (id) {
+  if (document.getElementById(id).value < 20 || document.getElementById(id).value > 200) {
+    document.getElementById(id).classList.remove("change-colours");
+  }
+  else {
+    document.getElementById(id).classList.add("change-colours");
+  }
+}
+
+//modal functions
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    document.querySelector(".modal-window").style.transform = "scale(0)";
+    setTimeout(function(){
+      modal.style.display = "none";
+    }, 300); //waiting during the animation duration of closing the modal
+
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    document.querySelector(".modal-window").style.transform = "scale(0)";
+    setTimeout(function(){
+      event.target.style.display = "none";
+    }, 300); //waiting during the animation duration of closing the modal
+  }
+}
+
+function generateConfirmationText () {
+
+  var corporation = document.getElementById("corporation").value;
+  var score = document.getElementById("corporation-score").value;
+  var expansions = arrayExpansions();
+  var result = document.querySelector("input[name='result']:checked").value;
+
+  document.getElementById("modalCorporation").innerHTML = corporation;
+  document.getElementById("modalScore").innerHTML = score;
+  document.getElementById("modalExpansions").innerHTML = expansions.toString().replace(/,/g, " - ");
+  document.getElementById("modalResult").innerHTML = result;
+}
+
+function closeModal () {
+  document.querySelector(".modal-window").style.transform = "scale(0)";
+  setTimeout(function(){
+    document.getElementById("modalOne").style.display = "none";
+  }, 300); //waiting during the animation duration of closing the modal
+}
+
+function enableSteps() {
+  document.querySelector("label[for='win']").style.width = "118px";
+  document.getElementById("loss_text").style.marginLeft = "-50px";
+  setTimeout(function() {
+      document.querySelector(".loss").style.backgroundPosition = "56px 0px";
+  },500);
+
+}
+
+
+function disableSteps() {
+  document.querySelector("label[for='win']").style.width = "240px";
+  document.getElementById("loss_text").style.marginLeft = "";
+  document.querySelector(".loss").style.backgroundPosition = "120px 0px";
+}
