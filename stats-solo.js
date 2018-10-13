e416c84f1298d6b2ebfe54ad5607760ac581b1b1
Simeon-off <s.simeonoff@gmail.com>
2018-10-13 23:10:44 +0200

    solo is live

--- stats-solo.js
+++ stats-solo.js
@@ -15,22 +15,91 @@ gamesRef.on('value', (snap) => {
   games = Object.keys(val)
     .map(key => val[key])
 ///////////////////////////////////////////////////////////////////////////////
-
-  compareTime()
-
+  pushData();
 });
 
-function getUTCtime() {
-  now = Math.floor((new Date()).getTime() / 1000)
+function filterFunction(id) {
+  clickedElementID = document.getElementById(id);
+  if (clickedElementID != null) {clickedElementID.classList.toggle("active");}
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-function compareTime() {
+function pushData() {
+  setTimeout(function() {pushGeneralStats()}, 500); //for smoother animation
+  pushHistory();
+
+}
+
+function pushGeneralStats() {
+  //total games
+  document.getElementById("total_games").innerHTML = games.length;
+}
+
+function pushHistory() {
+  //current time in seconds
   now = Math.floor((new Date()).getTime() / 1000);
-  timestamp = parseInt(games[games.length-1]["timestamp"]);
-  time = Math.floor((now - timestamp)/60); //get the minutes
-  if (time < 2) {console.log("NOW")}
-  if (time >= 2) {console.log(time + " minutes ago")}
-  if (time >= 60) {
-     hours = Math.floor(time/60)
-     console.log(hours + " hours ago")}
+  gameSections = document.querySelectorAll(".grid-cell-history2");
+
+  for(i=0; i < gameSections.length ; i++) {
+
+    //game timestamp in seconds
+    timestamp = games[games.length-1-i]["timestamp"];
+    if (timestamp == undefined) {time = "unknown"}
+    else {time = now - timestamp;}
+    gameSections[i].querySelector(".history-section-time-value").innerHTML = compareTime(time);
+
+    //the corporations array
+    corporationsSection = gameSections[i].querySelector(".history-section-corporation");
+    scoreSection = gameSections[i].querySelector(".history-section-score");
+    corporationsSection.innerHTML = games[games.length-1-i]["corporation"];
+
+    result = games[games.length-1-i]["result"];
+    if (result > 20) {scoreSection.innerHTML = result}
+    else {scoreSection.innerHTML = "<span class='failed-number'>"+result+"</span>"}
+
+
+    // the generations
+
+    //the expansions
+    var expansionsHTML = "";
+    expansionsArray = games[games.length-1-i]["expansions"];
+    if (expansionsArray == undefined) {expansionsArray = []}
+    if (expansionsArray.indexOf("CORPORATE") > -1) {
+      expansionsHTML = expansionsHTML + '<div class="history-section-expansion-ribbon"><div class="icon corporate-era-icon icon-align2"></div></div>'
+    }
+    if (expansionsArray.indexOf("VENUS") > -1) {
+      expansionsHTML = expansionsHTML + '<div class="history-section-expansion-ribbon"><div class="icon venus-icon icon-align2"></div></div>'
+    }
+    if (expansionsArray.indexOf("PRELUDE") > -1) {
+      expansionsHTML = expansionsHTML + '<div class="history-section-expansion-ribbon"><div class="icon prelude-icon icon-align2"></div></div>'
+    }
+    if (expansionsArray.indexOf("COLONIES") > -1) {
+      expansionsHTML = expansionsHTML + '<div class="history-section-expansion-ribbon"><div class="icon colonies-icon icon-align2"></div></div>'
+    }
+    gameSections[i].querySelector(".history-section-expansions").innerHTML = expansionsHTML;
+
+  }
+}
+
+function compareTime(time) {
+  if (time < 120) {return "just now"}
+  if (time >= 120 && time < 3600) {return Math.floor(time/60) + " minutes"}
+  if (time >= 3600) {return Math.floor(time/3600) + " hours"}
+}
+
+function changeColours (id) {
+  menu = document.getElementById(id);
+  if (document.querySelector("option[value='NOT SELECTED']").selected) {menu.classList.remove("change-colours");}
+  else {menu.classList.add("change-colours");}
 }
