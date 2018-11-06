b8e8366a1b0e78e4a5420c93fbf52ed0a619b9c9
Simeon-off <s.simeonoff@gmail.com>
2018-11-06 18:02:45 +0100

    ok

--- stats-solo.js
+++ stats-solo.js
@@ -158,3 +158,10 @@ function snapshotToArray(snapshot) {
     });
     return returnArr;
 };
+
+function compareTime(time) {
+  if (time >= 0 && time < 120) {return "now"}
+  if (time >= 120 && time < 7200) {return Math.floor(time/60) + " mins"}
+  if (time >= 7200 && time < 172800) {return Math.floor(time/3600) + " hours"}
+  if (time >= 172800) {return Math.floor(time/86400) + " days"}
+}
