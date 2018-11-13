2addf2ad9fbaad85209c6b00d70702b3b6d06cde
Simeon Simeonov <s.simeonoff@gmail.com>
2018-11-13 17:38:41 +0100

    ok

--- setup.js
+++ setup.js
@@ -17,18 +17,41 @@ function generateArray(length, max) {
 
 
 function displayCards() {
-  cards = generateArray(10, 261);
+  projects = generateArray(10, 261);
   corporations = generateArray(2, 30);
   preludes = generateArray(4, 35);
+
+  for (i=0; i < projects.length; i++) {
+    if (projects[i] < 10) {projects[i] = "00" + projects[i].toString()}
+    else if (projects[i] < 100) {projects[i] = "0" + projects[i].toString()}
+    else {projects[i] = projects[i].toString()}
+  }
+
+  for (i=0; i < preludes.length; i++) {
+    if (preludes[i] < 10) {preludes[i] = "P0" + preludes[i].toString()}
+    else {preludes[i] = "P" + preludes[i].toString()}
+  }
 
   //showing only the pointed cards
   for (i = 0; i < x.length; i++) {
     if (x[i].querySelector(".number") != null) {
-      if (cards.includes(x[i].querySelector(".number").textContent) || preludes.includes(x[i].querySelector(".number").textContent) ) {
+      if (projects.includes(x[i].querySelector(".number").textContent)) {
         x[i].style.display = "block";
         titles = titles + x[i].querySelector(".title").textContent + "<br>";
       }
     }
   }
+
+  titles = titles + "<br>"
+
+  for (i = 0; i < x.length; i++) {
+    if (x[i].querySelector(".number") != null) {
+      if (preludes.includes(x[i].querySelector(".number").textContent)) {
+        x[i].style.display = "block";
+        titles = titles + x[i].querySelector(".title").textContent + "<br>";
+      }
+    }
+  }
+
   document.getElementById("titles").innerHTML = titles;
 }
