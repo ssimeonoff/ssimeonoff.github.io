965b4fbec650badf309cf1b6addc0fb2b00a9a12
Simeon-off <s.simeonoff@gmail.com>
2018-11-15 00:52:37 +0100

    added filtering to setup

--- setup.js
+++ setup.js
@@ -41,11 +41,53 @@ function generateHand() {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  document.getElementById("link").classList.remove("disabled-link");
 
-  generateArray();
   displayCards();
 }
 
+
+
+
+
+function displayCards() {
+
+  queryProjects = ".filterDiv.standard.automated, .filterDiv.standard.active, .filterDiv.standard.events";
+  if (document.querySelectorAll(".active.corporate-icon").length > 0) {
+    queryProjects = queryProjects + ", .filterDiv.corporate.automated, .filterDiv.corporate.active, .filterDiv.corporate.events"
+  }
+  if (document.querySelectorAll(".active.venus-icon").length > 0) {
+    queryProjects = queryProjects + ", .filterDiv.venusNext.automated, .filterDiv.venusNext.active, .filterDiv.venusNext.events"
+  }
+  if (document.querySelectorAll(".active.prelude-icon").length > 0) {
+    queryProjects = queryProjects + ", .filterDiv.prelude.automated, .filterDiv.prelude.active, .filterDiv.prelude.events"
+  }
+  if (document.querySelectorAll(".active.colonies-icon").length > 0) {
+    queryProjects = queryProjects + ", .filterDiv.colonies.automated, .filterDiv.colonies.active, .filterDiv.colonies.events"
+  }
+  if (document.querySelectorAll(".active.promo-icon").length > 0) {
+    queryProjects = queryProjects + ", .filterDiv.promo.automated, .filterDiv.promo.active, .filterDiv.promo.events"
+  }
+
+  projects = document.querySelectorAll(queryProjects);
+  console.log(projects.length)
+
+  projectsArray = generateArray(10, projects.length)
+
+
+  for (i=0; i<10; i++) {
+    //display the 10 projects
+    projects[projectsArray[i]].style.display = "block"
+  }
+
+}
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
 function filterFunction(id) {
   clickedElementID = document.getElementById(id);
   if (clickedElementID != null) {clickedElementID.classList.toggle("active");}
@@ -54,67 +96,9 @@ function filterFunction(id) {
 function generateArray(length, max) {
   var arr = [];
   while(arr.length < length){
-      var randomnumber = Math.floor(Math.random()*max) + 1;
+      var randomnumber = Math.floor(Math.random()*max);
       if(arr.indexOf(randomnumber) > -1) continue;
       arr[arr.length] = randomnumber;
   }
   return arr
-}
-
-
-function displayCards() {
-  projects = generateArray(10, 261);
-  corporations = generateArray(2, 30);
-  preludes = generateArray(4, 35);
-  var titles_corporations = "";
-  var titles_projects = "";
-  var titles_preludes = "";
-
-  //appending the url
-  document.getElementById("link").href = url;
-
-  //corporations
-  for (i=0; i < corporations.length; i++) {
-    titles_corporations = titles_corporations + corporations_names[corporations[i]] + "<br>";
-    if (corporations[i] < 10) {corporations[i] = "CORP0" + corporations[i].toString()}
-    else  {corporations[i] = "CORP" + corporations[i].toString()}
-    url = url + "#" + corporations[i];
-  }
-  for (i=0; i < corporations.length; i++) {
-    document.getElementById(corporations[i]).style.display = "block";
-  }
-  document.getElementById("titles_corporations").innerHTML = titles_corporations;
-
-  //projects
-  for (i=0; i < projects.length; i++) {
-    if (projects[i] < 10) {projects[i] = "00" + projects[i].toString()}
-    else if (projects[i] < 100) {projects[i] = "0" + projects[i].toString()}
-    else {projects[i] = projects[i].toString()}
-    url = url + "#" + projects[i];
-  }
-  for (i = 0; i < x.length; i++) {
-    if (x[i].querySelector(".number") != null) {
-      if (projects.includes(x[i].querySelector(".number").textContent)) {
-        x[i].style.display = "block";
-        titles_projects = titles_projects + x[i].querySelector(".title").textContent + "<br>";
-      }
-    }
-  }
-  document.getElementById("titles_projects").innerHTML = titles_projects;
-
-  //preludes
-  for (i=0; i < preludes.length; i++) {
-    if (preludes[i] < 10) {preludes[i] = "P0" + preludes[i].toString()}
-    else {preludes[i] = "P" + preludes[i].toString()}
-    url = url + "#" + preludes[i];
-  }
-  for (i = 0; i < x.length; i++) {
-    if (x[i].querySelector(".number") != null) {
-      if (preludes.includes(x[i].querySelector(".number").textContent)) {
-        x[i].style.display = "block";
-        titles_preludes = titles_preludes + x[i].querySelector(".title").textContent + "<br>";
-      }
-    }
-  }
-  document.getElementById("titles_preludes").innerHTML = titles_preludes;
 }
