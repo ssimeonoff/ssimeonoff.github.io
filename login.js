1d4cefccfe959a52a8a59179fd68615dd4c4fd88
Simeon-off <s.simeonoff@gmail.com>
2018-12-05 21:57:14 +0100

    ok

--- login.js
+++ login.js
@@ -11,7 +11,7 @@
       firebase.initializeApp(config);
       // FirebaseUI config.
       var uiConfig = {
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInSuccessUrl: 'stats',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID
