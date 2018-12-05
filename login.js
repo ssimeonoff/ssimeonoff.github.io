cd40d9564419e4988b0d2359f7d3425f0c21e4b6
Simeon Simeonov <s.simeonoff@gmail.com>
2018-12-05 16:59:39 +0100

    login page

---
+++ login.js
@@ -0,0 +1,32 @@
+
+      var config = {
+        apiKey: "AIzaSyD6HEAHfcXGN-WrUxSaraO3TYNzGbAr8ts",
+        authDomain: "tm-games1.firebaseapp.com",
+        databaseURL: "https://tm-games1.firebaseio.com",
+        projectId: "tm-games1",
+        storageBucket: "tm-games1.appspot.com",
+        messagingSenderId: "969120080569"
+      };
+
+      firebase.initializeApp(config);
+      // FirebaseUI config.
+      var uiConfig = {
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        // tosUrl and privacyPolicyUrl accept either url string or a callback
+        // function.
+        // Terms of service url/callback.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url/callback.
+        privacyPolicyUrl: function() {
+          window.location.assign('<your-privacy-policy-url>');
+        }
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
