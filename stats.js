619d77bd9d0eb699911cd00e1221789ffaf20c5a
Simeon Simeonov <s.simeonoff@gmail.com>
2018-12-05 15:29:42 +0100

    added toggle plus new ui

--- stats.js
+++ stats.js
@@ -90,29 +90,56 @@ function filterFunction(id) {
 
   //filter by expansions
   btnExpansion = document.querySelectorAll(".btn-expansion.active");
-
-  if (btnExpansion.length == 1 ) {
-    games = games.filter(function(el) {
-      return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 && el.expansions.length == 1
-    });
-  } else if (btnExpansion.length == 2 ){
-    games = games.filter(function(el) {
-      return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 && el.expansions.length == 2
-    });
-  } else if (btnExpansion.length == 3 ){
-    games = games.filter(function(el) {
-      return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[2].id) > -1 && el.expansions.length == 3
-    });
-  } else if (btnExpansion.length == 4 ){
-    games = games.filter(function(el) {
-      return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[2].id) > -1 &&
-      el.expansions != undefined  && el.expansions.indexOf(btnExpansion[3].id) > -1 && el.expansions.length == 4
-    });
+  if (document.getElementById("expansions_switch").checked) {
+    //if the toggle EXCLUDE is on
+    if (btnExpansion.length == 1 ) {
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) == -1 ||
+               el.expansions == undefined
+      });
+    } else if (btnExpansion.length == 2 ){
+      games = games.filter(function(el) {
+          return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) == -1 &&
+                 el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[1].id) == -1 ||
+                 el.expansions == undefined
+      });
+    } else if (btnExpansion.length == 3 ){
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) == -1 &&
+               el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[1].id) == -1 &&
+               el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[2].id) == -1 ||
+               el.expansions == undefined
+      });
+    } else if (btnExpansion.length == 4 ){
+      games = games.filter(function(el) {
+        return el.expansions == undefined
+      });
+    }
+  } else {
+    //if the toggle INCLUDE is on
+    if (btnExpansion.length == 1 ) {
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 && el.expansions.length == 1
+      });
+    } else if (btnExpansion.length == 2 ){
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 && el.expansions.length == 2
+      });
+    } else if (btnExpansion.length == 3 ){
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[2].id) > -1 && el.expansions.length == 3
+      });
+    } else if (btnExpansion.length == 4 ){
+      games = games.filter(function(el) {
+        return el.expansions != undefined  &&  el.expansions.indexOf(btnExpansion[0].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[1].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[2].id) > -1 &&
+        el.expansions != undefined  && el.expansions.indexOf(btnExpansion[3].id) > -1 && el.expansions.length == 4
+      });
+    }
   }
 
   //filter by draft
