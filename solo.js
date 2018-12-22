f7a8dc3c715f4b31e116f4bf045c3d976f7a19c6
Simeon Simeonov <s.simeonoff@gmail.com>
2018-12-22 18:35:28 +0100

    ok

--- solo.js
+++ solo.js
@@ -44,8 +44,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log("logged")
     document.getElementById("title-auth").innerHTML = user.displayName + " - " + user.email
     document.getElementById("mygames").disabled = false;
-    if (user.email == "s.ssimeonoff@gmail.com") {
-      console.log("majk")
+    if (user.email == "s.simeonoff@gmail.com") {
       document.getElementById("lossSection").classList.add("boyMajk");
     }
   } else {
