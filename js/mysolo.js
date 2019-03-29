556ba239581337267418ee5a307abbd811274cd7
Simeon Simeonov <s.simeonoff@gmail.com>
2019-03-29 11:57:17 +0100

    refactor

--- js/mysolo.js
+++ js/mysolo.js
@@ -1,5 +1,3 @@
-histogramPrelude();
-histogramColonies();
 
 data_ce = [
   ["Credicor", 108,94,87,89,0,95,82,89,107,106,106,113,110],
@@ -16,7 +14,6 @@ data_ce = [
   ["Saturn Systems", 79,94,0,91,89,132,99,88,91,0,0,95,81]
 
 ];
-histogram("div_ce", data_ce);
 
 data_ce_vn = [
   ["Credicor", 0,77,112,97,0,94,108,103,99,92,0,92],
@@ -37,203 +34,119 @@ data_ce_vn = [
   ['Viron', 105,94,96],
   ['MSI', 93,106,101,94,104,104,108,91]
 ];
+
+data_ce_vn_pr = [
+  ['Credicor', 82],
+  ['Ecoline', 111,96,0,99,109,102,96,114,93,98,117],
+  ['Helion', 96,92,0,85,87,101,98],
+  ['Interplanetary', 83,0],
+  ['Inventrix', 88,85,88,0,105],
+  ['Phobolog', 110,96,100,73,101,107,0,87,97],
+  ['Tharsis Republic', 80,112,89,0],
+  ['Thorgate', 0,88],
+  ['UNMI', 92,117,98,95],
+  ['Teractor', 104,100],
+  ['Saturn Systems', 105,97,111,106,89],
+  ['Aphrodite', 79,96],
+  ['Celestic', 0,88,89,115,102,0],
+  ['Manutech', 103,0,100],
+  ['MSI', 0,84,0,0,95,119,88,0,90],
+  ['Viron', 88,106,91,107],
+  ['Cheung Shing Mars', 92,0,97,103,90,87,84,98,108],
+  ['Point Luna', 106],
+  ['Robinson Industries', 107,88,102,0,95,90,104],
+  ['Valley Trust', 100,0,97,118,87],
+  ['Vitor', 0,0]
+];
+
+data_ce_vn_pr_co = [
+  ['Credicor', 85,104,103,105],
+  ['Ecoline', 0],
+  ['Helion', 0,102],
+  ['Mining Guild', 0],
+  ['Interplanetary', 97,99,98,114],
+  ['Inventrix', 70,109],
+  ['Phobolog', 0],
+  ['Tharsis Republic'],
+  ['Thorgate'],
+  ['UNMI', 90,108,101],
+  ['Teractor'],
+  ['Celestic', 80,99,0,106,0],
+  ['Manutech', 97,0],
+  ['MSI', 95,99,97],
+  ['Viron', 104],
+  ['Cheung Shing Mars', 99],
+  ['Point Luna'],
+  ['Robinson Industries', 88,90,90],
+  ['Valley Trust', 90,107,110],
+  ['Vitor', 91],
+  ['Aridor', 114,116,110],
+  ['Polyphemos', 99,80,100],
+  ['Poseidon', 87,108],
+  ['Polyphemos'],
+  ['Stormcraft', 90,113]
+];
+
+histogram("div_ce", data_ce);
 histogram("div_ce_vn", data_ce_vn);
+histogram("div_ce_vn_pr", data_ce_vn_pr);
+histogram("div_ce_vn_pr_co", data_ce_vn_pr_co);
 
-
+/////////////////////////////////////////////////////
 function histogram (div_id, data) {
-  
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
 
-      var options = {
-          title: '',
-          titleTextStyle: {fontSize:20, color:"#444444"},
-          legend: { position: 'none' },
-          fontSize: 12,
-          backgroundColor: "transparent",
-          vAxis: { gridlines: { count: 1}, maxValue:20 },
-          hAxis: {textStyle : {fontSize: 10, fontName: 'Prototype'}},
-          bar: {gap: 1},
-          chartArea:{left:0,bottom:20,top:0,width:380},
-          colors: ['#444444','#888888'],
-          histogram: {bucketSize: 5, minValue: 75, maxValue: 135}
-        };
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
 
-        var games = [
-          ['Corporation', 'Score'],
-        ];
+  var options = {
+      title: '',
+      titleTextStyle: {fontSize:20, color:"#444444"},
+      legend: { position: 'none' },
+      fontSize: 12,
+      backgroundColor: "transparent",
+      vAxis: { gridlines: { count: 1}, maxValue:20 },
+      hAxis: {textStyle : {fontSize: 10, fontName: 'Prototype'}},
+      bar: {gap: 1},
+      chartArea:{left:0,bottom:20,top:0,width:380},
+      colors: ['#444444','#888888'],
+      histogram: {bucketSize: 5, minValue: 75, maxValue: 135}
+    };
 
-        pushGamesData(games, data)
-        generateHTML(div_id, games);
-        div = document.getElementById(div_id)
+    var games = [
+      ['Corporation', 'Score'],
+    ];
 
-        var losses = removeZeros(games);
-        var sum = 0;
-        for( var i = 1; i < games.length; i++ ){
-          sum += parseInt(games[i][1]); //don't forget to add the base
-        }
-        var avg = Math.round(parseFloat(sum/(games.length-1)));
-        var winrate = Math.round(parseFloat(100*(games.length-1)/(games.length-1+losses)));
+    pushGamesData(games, data)
+    generateHTML(div_id, games);
+    div = document.getElementById(div_id)
 
-        div.querySelector(".wins-mysolo").innerHTML = games.length -1 + " <span style='font-size:20px;font-weight:bold'>&#x2713;</span>";
-        div.querySelector(".points-mysolo").innerHTML = avg + " <span style='font-size:14px'>P</span>";
-        div.querySelector(".losses-mysolo").innerHTML = losses + " <span style='font-size:20px;'>&#x2717;</span>";
-        div.querySelector(".winrate-mysolo").innerHTML = " " + winrate + " <span style='font-size:14px'>%</span>";
+    var losses = removeLosses(games);
+    var sum = 0;
+    for( var i = 1; i < games.length; i++ ){
+      sum += parseInt(games[i][1]); //don't forget to add the base
+    }
+    var avg = Math.round(parseFloat(sum/(games.length-1)));
+    var winrate = Math.round(parseFloat(100*(games.length-1)/(games.length-1+losses)));
+
+    div.querySelector(".wins-mysolo").innerHTML = games.length -1 + " <span style='font-size:20px;font-weight:bold'>&#x2713;</span>";
+    div.querySelector(".points-mysolo").innerHTML = avg + " <span style='font-size:14px'>P</span>";
+    div.querySelector(".losses-mysolo").innerHTML = losses + " <span style='font-size:20px;'>&#x2717;</span>";
+    div.querySelector(".winrate-mysolo").innerHTML = " " + winrate + " <span style='font-size:14px'>%</span>";
 
     var games = google.visualization.arrayToDataTable(games);
     var chart = new google.visualization.Histogram(div.querySelector(".chart-div-mysolo"));
     chart.draw(games, options);
+
   }
 }
 
-function histogramPrelude () {
-  google.charts.load("current", {packages:["corechart"]});
-  google.charts.setOnLoadCallback(drawChart);
-  function drawChart() {
-
-  var options = {
-      title: '',
-      titleTextStyle: {fontSize:20, color:"#444444"},
-      legend: { position: 'none' },
-      fontSize: 12,
-      backgroundColor: "transparent",
-      vAxis: { gridlines: { count: 1}, maxValue:20 },
-      hAxis: {textStyle : {fontSize: 10, fontName: 'Prototype'}},
-      bar: {gap: 1},
-      chartArea:{left:0,bottom:20,top:0,width:380},
-      colors: ['#444444','#888888'],
-      histogram: {bucketSize: 5, minValue: 70, maxValue: 135}
-    };
-
-    //PRELUDE GAMES
-    games = [
-          ['Corporation', 'Score'],
-
-          ['Credicor', 82],
-          ['Ecoline', 111],['Ecoline', 96],['Ecoline', 0],['Ecoline', 99],['Ecoline', 109],['Ecoline', 102],['Ecoline', 96],['Ecoline', 114],['Ecoline', 93],['Ecoline', 98],['Ecoline', 117],
-          ['Helion', 96],['Helion', 92],['Helion', 0],['Helion', 85],['Helion', 87],['Helion', 101],['Helion', 98],
-
-          ['Interplanetary', 83], ['Interplanetary', 0],
-          ['Inventrix', 88],['Inventrix', 85],['Inventrix', 88],['Inventrix', 0],['Inventrix', 105],
-          ['Phobolog', 110],['Phobolog', 96],['Phobolog', 100],['Phobolog', 73],['Phobolog', 101],['Phobolog', 107],['Phobolog', 0],['Phobolog', 87],['Phobolog', 97],
-          ['Tharsis Republic', 80],['Tharsis Republic', 112],['Tharsis Republic', 89],['Tharsis Republic', 0],
-          ['Thorgate', 0],['Thorgate', 88],
-          ['UNMI', 92],['UNMI', 117],['UNMI', 98],['UNMI', 95],
-          ['Teractor', 104],['Teractor', 100],
-          ['Saturn Systems', 105],['Saturn Systems', 97],['Saturn Systems', 111],['Saturn Systems', 106],['Saturn Systems', 89],
-          ['Aphrodite', 79],['Aphrodite', 96],
-          ['Celestic', 0],['Celestic', 88],['Celestic', 89],['Celestic', 115],['Celestic', 102],['Celestic', 0],
-          ['Manutech', 103],['Manutech', 0],['Manutech', 100],
-          ['MSI', 0],['MSI', 84],['MSI', 0],['MSI', 0],['MSI', 95],['MSI', 119],['MSI', 88],['MSI', 0],['MSI', 90],
-          ['Viron', 88],['Viron', 106],['Viron', 91],['Viron', 107],
-          ['Cheung Shing Mars', 92],['Cheung Shing Mars', 0],['Cheung Shing Mars', 97],['Cheung Shing Mars', 103],['Cheung Shing Mars', 90],['Cheung Shing Mars', 87],['Cheung Shing Mars', 84],['Cheung Shing Mars', 98],['Cheung Shing Mars', 108],
-          ['Point Luna', 106],
-          ['Robinson Industries', 107],['Robinson Industries', 88], ['Robinson Industries', 102],['Robinson Industries', 0],['Robinson Industries', 95],['Robinson Industries', 90],['Robinson Industries', 104],
-          ['Valley Trust', 100],['Valley Trust', 0],['Valley Trust', 97],['Valley Trust', 118],['Valley Trust', 87],
-          ['Vitor', 0],['Vitor', 0]
-
-        ];
-
-
-        generateHTML("prelude_div",games);
-
-        var losses = removeZeros(games);
-        var sum = 0;
-        for( var i = 1; i < games.length; i++ ){
-          sum += parseInt(games[i][1]); //don't forget to add the base
-        }
-        var avg = Math.round(parseFloat(sum/(games.length-1)));
-        var winrate = Math.round(parseFloat(100*(games.length-1)/(games.length-1+losses)));
-
-
-        document.getElementById("wins_prelude").innerHTML = games.length -1 + " <span style='font-size:20px;font-weight:bold'>&#x2713;</span>";
-        document.getElementById("points_prelude").innerHTML = avg + " <span style='font-size:14px'>P</span>";
-        document.getElementById("losses_prelude").innerHTML = losses + " <span style='font-size:20px;'>&#x2717;</span>";
-        document.getElementById("winrate_prelude").innerHTML = " " + winrate + " <span style='font-size:14px'>%</span>";
-
-    var games = google.visualization.arrayToDataTable(games);
-    var chart = new google.visualization.Histogram(document.getElementById('histogram_prelude'));
-    chart.draw(games, options);
-  }
-}
-
-function histogramColonies () {
-  google.charts.load("current", {packages:["corechart"]});
-  google.charts.setOnLoadCallback(drawChart);
-  function drawChart() {
-
-  var options = {
-      title: '',
-      titleTextStyle: {fontSize:20, color:"#444444"},
-      legend: { position: 'none' },
-      fontSize: 12,
-      backgroundColor: "transparent",
-      vAxis: { gridlines: { count: 1}, maxValue:20 },
-      hAxis: {textStyle : {fontSize: 10, fontName: 'Prototype'}},
-      bar: {gap: 1},
-      chartArea:{left:0,bottom:20,top:0,width:380},
-      colors: ['#444444','#888888'],
-      histogram: {bucketSize: 5, minValue: 70, maxValue: 135}
-    };
-
-    //COLONIES GAMES
-    games = [
-          ['Corporation', 'Score'],
-
-          ['Credicor', 85],['Credicor', 104],['Credicor', 103],['Credicor', 105],
-          ['Ecoline', 0],
-          ['Helion', 0], ['Helion', 102],
-          ['Mining Guild', 0],
-          ['Interplanetary', 97],['Interplanetary', 99],['Interplanetary', 98],['Interplanetary', 114],
-          ['Inventrix', 70],['Inventrix', 109],
-          ['Phobolog', 0],
-
-
-          ['UNMI', 90],['UNMI', 108],['UNMI', 101],
-
-          ['Celestic', 80],['Celestic', 99],['Celestic', 0],['Celestic', 106],['Celestic', 0],
-          ['Manutech', 97],['Manutech', 0],
-          ['MSI', 95], ['MSI', 99],['MSI', 97],
-          ['Viron', 104],
-          ['Cheung Shing Mars', 99],
-
-          ['Robinson Industries', 88], ['Robinson Industries', 90],['Robinson Industries', 90],
-          ['Valley Trust', 90], ['Valley Trust', 107],['Valley Trust', 110],
-          ['Vitor', 91],
-          ['Aridor', 114], ['Aridor', 116],['Aridor', 110],
-          ['Polyphemos', 99],['Polyphemos', 80],['Polyphemos', 100],
-          ['Poseidon', 87], ['Poseidon', 108],
-
-          ['Stormcraft', 90],['Stormcraft', 113]
-        ];
-
-        generateHTML("colonies_div",games);
-
-        var losses = removeZeros(games);
-        var sum = 0;
-        for( var i = 1; i < games.length; i++ ){
-          sum += parseInt(games[i][1]); //don't forget to add the base
-        }
-        var avg = Math.round(parseFloat(sum/(games.length-1)));
-        var winrate = Math.round(parseFloat(100*(games.length-1)/(games.length-1+losses)));
-
-
-        document.getElementById("wins_colonies").innerHTML = games.length -1 + " <span style='font-size:20px;font-weight:bold'>&#x2713;</span>";
-        document.getElementById("points_colonies").innerHTML = avg + " <span style='font-size:14px'>P</span>";
-        document.getElementById("losses_colonies").innerHTML = losses + " <span style='font-size:20px;'>&#x2717;</span>";
-        document.getElementById("winrate_colonies").innerHTML = " " + winrate + " <span style='font-size:14px'>%</span>";
-
-    var games = google.visualization.arrayToDataTable(games);
-    var chart = new google.visualization.Histogram(document.getElementById('histogram_colonies'));
-    chart.draw(games, options);
-  }
-}
-
-function removeZeros(games) {
+///////////////////////////////////////////////////
+function removeLosses(games) {
   var losses = 0
   for (i=0; i < games.length;i++ ) {
-    if (parseInt(games[i][1]) == 0) {
+    //score under 70 is counted as a loss
+    if (parseInt(games[i][1]) < 70) {
       games.splice(i,1);
       i--;
       losses++;
@@ -242,37 +155,31 @@ function removeZeros(games) {
   return losses
 }
 
-function generateHTML(expansions,games) {
-  expansionsDiv = document.getElementById(expansions)
-  for (i=1; i < games.length; i++ ) {
-    var el = expansionsDiv.querySelector("div[title='"+games[ i][0]+"'] > div[class='empty']")
-    if (games[i][1] > 99) {
-      el.innerHTML = games[i][1];
-      el.classList.add("result");
-      el.classList.add("background-gold");
-    }
-    if (games[i][1] > 79 && games[i][1] < 100) {
-      el.innerHTML = games[i][1];
-      el.classList.add("result");
-      el.classList.add("background-silver");
-    }
-    if (games[i][1] > 0 && games[i][1] < 80) {
-      el.innerHTML = games[i][1];
-      el.classList.add("result");
-      el.classList.add("background-bronze");
-    }
-    if (games[i][1] == 0 ) {
-      el.classList.add("failed");
-    }
-
-    el.classList.remove("empty");
-  }
-}
-
 function pushGamesData (main_arr, data_arr) {
   for (i=0; i< data_arr.length; i++) {
     for (j=1; j <data_arr[i].length; j++) {
       main_arr.push([data_arr[i][0],data_arr[i][j]])
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////
+function generateHTML(expansions,games) {
+
+  expansionsDiv = document.getElementById(expansions);
+  for (i=1; i < games.length; i++ ) {
+    var el = expansionsDiv.querySelector("div[title='"+games[i][0]+"']");
+    if (games[i][1] > 99) {
+      el.innerHTML += "<div class='result background-gold'>"+games[i][1]+"</div>";
+    }
+    if (games[i][1] > 79 && games[i][1] < 100) {
+      el.innerHTML += "<div class='result background-silver'>"+games[i][1]+"</div>";
+    }
+    if (games[i][1] > 0 && games[i][1] < 80) {
+      el.innerHTML += "<div class='result background-bronze'>"+games[i][1]+"</div>";
+    }
+    if (games[i][1] == 0 ) {
+      el.innerHTML += "<div class='failed'></div>";
     }
   }
 }
