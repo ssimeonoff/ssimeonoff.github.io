2d151faa5383ba7ffe1239745bbf90de1ac440a7
Simeon Simeonov <s.simeonoff@gmail.com>
2022-06-30 21:59:17 +0200

    merger

--- js/myscripts.js
+++ js/myscripts.js
@@ -20,7 +20,7 @@ function showAll() {
   var x, i;
   displayedProjects = 371;
   displayedCorporations = 40;
-  displayedPreludes = 36;
+  displayedPreludes = 37;
   displayedColonies = 11;
   displayedGlobals = 36;
 
